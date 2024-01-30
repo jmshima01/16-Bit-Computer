d3b1d4fe91e2d5b9f4aadc996e446ea8699085fb
jmshima01 <jamesshima@mines.edu>
2024-01-30 15:39:45 -0700

    added python scripting for nway gate creation and carry eq gen for n carry

--- carrylookahead.py
+++ carrylookahead.py
@@ -1,25 +1,79 @@
 # By James Shima
-# python script to make hdl for lookahead adder...
+# python script to make hdl generation easy for lookahead adder...
+from sys import argv
+
+
+def makeNWayGate(gate, C, *args):
+    c = 0
+    print(f"//{args}")
+    
+    if c==0:
+        print(f"{gate}(a={args[c]},b={args[c+1]},out=C{C}-{c});")
+        
+    for i in range(2,len(args)):
+        print(f"{gate}(a={args[i]},b=C{C}-{c},out=C{C}-{c+1});")
+        c+=1
+
+    """ Less efficent recursive algorithm I made """
+    # if len(args)%2==0:
+    #     for i in range(0,len(args)-1,2):
+    #         print(f"{gate}(a={args[i]},b={args[i+1]},out=C{C}-{c});")
+    #         vars.append(f"C{C}-{c}")
+    #         c+=1
+    #     print()
+    # else:
+        
+    #     for i in range(0,len(args)-1,2):
+    #         print(f"{gate}(a={args[i]},b={args[i+1]},out=C{C}-{c});")
+    #         vars.append(f"C{C}-{c}")
+    #         c+=1
+    #     print(f"{gate}(a={args[-1]},b=C{C}-{c},out=C{C}-{c+1});")
+    #     vars.append(f"C{C}-{c}")
+    #     vars.append(f"C{C}-{c+1}")
+    #     c+=2
+    #     print()
+
+    # makeNWayGate(gate, C, c, *vars)
+
+def generateCarryEq(C_iplus1):
+    res = "Cin*"
+    l = []
+    for i in list(range(C_iplus1))[::-1]:
+        if i == 0:
+            res+=f"P{i}"
+        else:
+            res+=f"P{i}*"
+        l.append(f"P{i}")
+    res+=" + "
+    l.pop(-1)
+    G = C_iplus1-1
+    for i in range(C_iplus1):
+        if i == G:
+            res+=f"G{i}"
+            continue
+        res+=f"G{i}"+ "*"+"*".join(l)
+        res+=" + "
+        l.pop(-1)
+
+    print(" + ".join(res.split(" + ")[::-1]))  
+
+
+    
+
+
+    
+
+
+
 if __name__ == "__main__":
     # C_i+1 = (A_i and B_i) or (C_i and (A_i ^ B_i))
-    
-    
 
-    def genCarry(C):
-        r = f"G{C-1} + "
-        for i in range(C-1):
-            r += 
+    # if len(argv)<=4:
+    #     print("useage: python3 (A-Z)+.py gate carry_id input1 input2 ... inputN")
 
-        return r 
-            
-            
+    # makeNWayGate(argv[1],argv[2],*argv[3:])
+    generateCarryEq(16)
 
-
-    for i in range(4):
-        # print(f"Xor(a=a[{i}],b=b[{i}],out=P{i})")
-        # print(f"And(a=a[{i}],b=b[{i}],out=G{i})")
-
-        print(f"FullAdder(a=a[{i}], b=b[{i}], c=C{i+1}, sum=out[{i}], carry=False)")
 
 
 
