7b81abbb6fe064f5e9f4b3a87ef204b81d387c58
jmshima01 <jamesshima@mines.edu>
2024-01-31 03:24:26 -0700

    done 02

---
+++ README.md
@@ -0,0 +1,4 @@
+1. James Shima, https://www.geeksforgeeks.org/carry-look-ahead-adder/
+2. I spent many hours on this project
+3. I was able to impliment a 16 bit carry look ahead adder!  Its over 1000 lines/gates long. I used python (carrylookahead.py) to make it if you are curious how I was able to do it. Maybe I'll also make a script to convert it to only nand down the line. 
+`python3 carrylookahead.py >> Add16.hdl`
--- carrylookahead.py
+++ carrylookahead.py
@@ -1,14 +1,16 @@
-# @author James Shima, CSCI410
-# python script to make hdl generation easy for 16 bit lookahead adder...
-from sys import argv
-
-
-
+""" 
+@author James Shima, CSCI410 - Dr. Romig III
+python script to make hdl generation easy 
+for 16 bit lookahead adder as it's alot of gates... 
+(over 1000 without n-way ands and ors)
+"""
+# Xors and Ands of A_i B_i
 def genPandG(bits):
     for i in range(bits):
         print(f"Xor(a=a[{i}],b=b[{i}],out=P{i});")
         print(f"And(a=a[{i}],b=b[{i}],out=G{i});")
 
+# prints out an N way gate using only single gates
 def makeNWayGate(gate, C, c, isEnd, *args):
     if len(args) < 2:
         return None
@@ -28,27 +30,10 @@ def makeNWayGate(gate, C, c, isEnd, *args):
         l.append(f"{gate}(a={args[i]},b=C{C}-{c},out=C{C}-{c+1});")
         c+=1
     return c+1,l
-    """ Less efficent recursive algorithm also works """
-    # if len(args)%2==0:
-    #     for i in range(0,len(args)-1,2):
-    #         print(f"{gate}(a={args[i]},b={args[i+1]},out=C{C}-{c});")
-    #         vars.append(f"C{C}-{c}")
-    #         c+=1
-    #     print()
-    # else:   
-    #     for i in range(0,len(args)-1,2):
-    #         print(f"{gate}(a={args[i]},b={args[i+1]},out=C{C}-{c});")
-    #         vars.append(f"C{C}-{c}")
-    #         c+=1
-    #     print(f"{gate}(a={args[-1]},b=C{C}-{c},out=C{C}-{c+1});")
-    #     vars.append(f"C{C}-{c}")
-    #     vars.append(f"C{C}-{c+1}")
-    #     c+=2
-    #     print()
-    # makeNWayGate(gate, C, c, *vars)
 
+# prints the carry equation for a given carry bit number
+# C_i+1 = (A_i and B_i) or (C_i and (A_i ^ B_i))
 def generateCarryEq(C_iplus1):
-    # C_i+1 = (A_i and B_i) or (C_i and (A_i ^ B_i))
     if C_iplus1 < 1:
         return None
     
@@ -72,17 +57,17 @@ def generateCarryEq(C_iplus1):
         l.pop(-1)
 
     x = " + ".join(res.split(" + ")[::-1])
-    # print(x)
     return x  
 
+# puts it all tother and prints out the carry equation in hack hdl form
 def generateCarryHdl(C):
     eq = generateCarryEq(C)
-    # print(eq)
+
     print(f"//C{C}:")
     l = eq.split(" + ")
     for i in range(len(l)):
         l[i] = l[i].split('*')
-    # print(l)
+
     c=0
     end_of_gate_blk =[]
     for i in range(len(l)):
@@ -91,12 +76,11 @@ def generateCarryHdl(C):
         c,a = makeNWayGate("And",C,c,False,*l[i])
         print()
         end_of_gate_blk.append(a[-1].split("out=")[-1][:-2])
-        # print(end_of_gate_blk)
+
     end_of_gate_blk.append(f"G{C-1}")
     c,_ = makeNWayGate("Or",C,c,True,*end_of_gate_blk)
 
-    # print(end_of_gate_blk)
-
+# prints the full adders the carrys go into
 def genFullAdders(C):
     # cin is not defined :( assume its 0 ig...
     print(f"FullAdder(a=a[0], b=b[0], c=false, sum=out[0], carry=false);")
@@ -105,19 +89,16 @@ def genFullAdders(C):
         print(f"FullAdder(a=a[{i}], b=b[{i}], c=C{i}, sum=out[{i}], carry=false);")
     print(f"FullAdder(a=a[{C-1}], b=b[{C-1}], c=C{C-1}, sum=out[{C-1}], carry=C{C});")
 
-
+# USAGE python3 carrylookahead.py >> Add16.hdl
+# or pipe result to a file of choice using > or >> or just stdout
 if __name__ == "__main__":
-    bits = 16
-    genPandG(bits)
-    for i in range(1,bits):
-        generateCarryHdl(i)
+    # bits = 16 # could make 32 or 64!
+    # genPandG(bits)
+    # for i in range(1,bits):
+    #     generateCarryHdl(i)    
+    # genFullAdders(16)
+    f = [f"outt[{i}]" for i in range(16)]
+    makeNWayGate("Or",1,0,False,*f)
+
+
     
-    genFullAdders(16)
-
-
-
-
-
-
-
-
